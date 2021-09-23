a489babd21ee314be6de8080591bc6812fc54d88
RazmusAnderson <computerchris101@gmail.com>
2021-09-23 17:39:57 +0100

    ffs

--- src/bot.py
+++ src/bot.py
@@ -13,12 +13,9 @@ args = {
   'TOKEN': None, 
   'MONGO_CONN_STR': None
 }
-i = 1
-# load from environment, if unable to then use positional args instead
-# this will break if you use a combination of envvars and args, use one or the other please thank you
 for arg in args:
   args[arg] = os.getenv(arg)
-
+print(args)
 print('Connecting to mongo...')
 quotes = Quotes(args['MONGO_CONN_STR'])
 print('Successful connection made to mongo')
