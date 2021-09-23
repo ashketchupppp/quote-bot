e60201f476c7f6aff061968c20b66ae844190da0
RazmusAnderson <computerchris101@gmail.com>
2021-09-23 15:57:47 +0100

    docker baby!

--- src/bot.py
+++ src/bot.py
@@ -1,26 +1,36 @@
 import os
+import sys
 import pathlib
 from discord.ext import commands
 from dotenv import load_dotenv
-import pymongo
 
 from commands import QuoteCommands
 from mongo import Quotes
 
 envPath = pathlib.Path(__file__).parent.parent.resolve()
 load_dotenv(envPath)
-TOKEN = os.getenv('TOKEN')
-MONGO_CONN_STR = os.getenv('MONGO_CONN_STR')
-
+args = {
+  'TOKEN': None, 
+  'MONGO_CONN_STR': None
+}
+i = 1
+# load from environment, if unable to then use positional args instead
+# this will break if you use a combination of envvars and args, use one or the other please thank you
+for arg in args:
+  args[arg] = os.getenv(arg)
+  if not args[arg]:
+    args[arg] = sys.argv[i]
+  i += 1
+print(args)
 print('Connecting to mongo...')
-quotes = Quotes(MONGO_CONN_STR)
+quotes = Quotes(args['MONGO_CONN_STR'])
 print('Successful connection made to mongo')
 
 bot = commands.Bot(command_prefix='q!')
 bot.add_cog(QuoteCommands(bot, quotes))
 
 try:
-  bot.run(TOKEN)
+  bot.run(args['TOKEN'])
 except KeyboardInterrupt:
   print('Exiting...')
 finally:
